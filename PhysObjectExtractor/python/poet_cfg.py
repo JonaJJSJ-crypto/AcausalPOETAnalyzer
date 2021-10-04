d3953072e6ed97a33b1108a2e3b00b03d2d0d936
JonaJJSJ-crypto <78813250+JonaJJSJ-crypto@users.noreply.github.com>
2021-10-04 10:24:53 -0500

    Update poet_cfg.py

--- PhysObjectExtractor/python/poet_cfg.py
+++ PhysObjectExtractor/python/poet_cfg.py
@@ -210,6 +210,7 @@ process.mygenparticle= cms.EDAnalyzer('GenParticleAnalyzer',
 				      #---- Collect particles with specific "status:pdgid"
                                       #---- Check PDG ID in the PDG.
 				      #---- if 0:0, collect them all
+				      #---- if 0:pdg, collect particle for all status
 				      input_particle = cms.vstring("1:11","0:23","1:-11","2:556","2:-556","1:22","0:24","0:-24","0:5","0:-5")
 				      )
 
